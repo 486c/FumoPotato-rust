15f114e073d619de34a4c4d1654a55319df0f680
486c <lopihavefun@gmail.com>
2023-01-13 12:34:13 +0300

    add migrations

--- migrations/20230113093058_init.sql
+++ migrations/20230113093058_init.sql
@@ -1,14 +1,15 @@
-create table twitch_streamers(
+-- Add migration script here
+CREATE TABLE IF NOT EXISTS twitch_streamers(
 	name varchar not null,
 	id bigint not null,
 	online boolean not null default false,
-	primary key(id),
+	primary key(id)
 );
 
-create table twitch_tracking(
+CREATE TABLE IF NOT EXISTS twitch_tracking(
 	channel_id bigint not null,
 	id bigint not null,
 	constraint fk_id
 		foreign key(id)
 			references twitch_streamers(id)
-);
+)
