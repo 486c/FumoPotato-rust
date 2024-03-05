4b185ca1cb33cfddf19b7f6314c349ad8af14a25
486c <lopihavefun@gmail.com>
2024-03-05 10:38:48 +0300

    fix: intoduce osu_tracked users table

---
+++ migrations/20240305072552_add-last-checked-time.sql
@@ -0,0 +1,8 @@
+-- Add migration script here
+
+create table osu_tracked_users(
+	osu_id int8 primary key not NULL,
+	last_checked timestamp not null
+)
+
+alter table osu_tracking add constraint fk_osu_id foreign key (osu_id) references osu_tracked_users(osu_id);
